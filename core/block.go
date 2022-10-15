ac2186a4841712152e183fb1bcd4e17418378118
Sunny <2732981250@qq.com>
2022-10-15 18:16:50 +0800

    2.1

--- core/block.go
+++ core/block.go
@@ -25,6 +25,18 @@ type Block struct {
 	Transactions []string
 }
 
+// MerkleNode MerkleTree 节点
+type MerkleNode struct {
+	Left  *MerkleNode
+	Right *MerkleNode
+	Data  []byte
+}
+
+// MerkleTree 根
+type MerkleTree struct {
+	RootNode *MerkleNode
+}
+
 // GenerateNewBlock 会生成一个新的区块并返回，具体执行流程包括：
 //
 // (1)生成一个 Block 区块实例，并将该实例的字段初始化
@@ -51,13 +63,20 @@ func GenerateNewBlock(previousBlock *Block, transactions []string) *Block {
 func CalculateHash(b *Block) string {
 	var hashValue string
 
-	record := string(b.Index) + string(b.TimeStamp) + b.PreviousHash
-	h := sha256.New()
-	h.Write([]byte(record))
-	hashed := h.Sum(nil)
-	//返回生成的Hash
-	hashValue = hex.EncodeToString(hashed)
+	//data := make([][]byte, len(b.Transactions))
+	//for i := range data {
+	//	data[i] = make([]byte, len([]byte(b.Transactions[0])))
+	//}
+	//for _, temp := range b.Transactions {
+	//	data = append(data, []byte(temp))
+	//}
 
+	//正确的代码应该是上面括号内的，但是总有错
+
+	data := [][]byte{[]byte("b.Transactions[0]"), []byte("b.Transactions[1]")}
+
+	tree := NewMerkleTree(data)
+	hashValue = hex.EncodeToString(tree.RootNode.Data)
 	return hashValue
 }
 
@@ -71,4 +90,66 @@ func GenerateGenesisBlock() *Block {
 	block.Transactions = []string{"***genesis block***"}
 	block.Hash = CalculateHash(block)
 	return block
+}
+
+// NewMerkleNode 创建Merkle节点
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	node := MerkleNode{}
+	// 创建存储明文信息的叶子节点
+	if left == nil && right == nil {
+		node.Data = data
+		// 创建只有一个分支的MerkleNode
+	} else if left != nil && right == nil {
+		hash := sha256.Sum256(left.Data)
+		node.Data = hash[:]
+		// 创建有两个分支的MerkleNode
+	} else {
+		// slice = append(slice, anotherSlice...) 两个slice拼接在一起时要加...
+		hash := sha256.Sum256(append(left.Data, right.Data...))
+		node.Data = hash[:]
+	}
+	node.Left = left
+	node.Right = right
+
+	return &node
+}
+
+func NewMerkleTree(data [][]byte) *MerkleTree {
+	var nodes []MerkleNode
+
+	// 将所有数据构建为dataNode节点，接入node节点的左分支，并将node节存到nodes数组中
+	for _, datum := range data {
+		dataNode := NewMerkleNode(nil, nil, datum)
+		node := NewMerkleNode(dataNode, nil, nil)
+		nodes = append(nodes, *node)
+	}
+
+	for {
+		var newLevel []MerkleNode
+
+		// 根据当前层的节点，构造上一层
+		// 当前层节点为奇数时
+		if len(nodes)%2 == 1 {
+			for j := 0; j < len(nodes)-1; j += 2 {
+				node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+				newLevel = append(newLevel, *node)
+			}
+			node := NewMerkleNode(&nodes[len(nodes)-1], nil, nil)
+			newLevel = append(newLevel, *node)
+			// 当前层节点为偶数时
+		} else {
+			for j := 0; j < len(nodes); j += 2 {
+				node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
+				newLevel = append(newLevel, *node)
+			}
+		}
+
+		// 更新层节点
+		nodes = newLevel
+		if len(nodes) == 1 {
+			break
+		}
+	}
+	mTree := MerkleTree{&nodes[0]}
+	return &mTree
 }
