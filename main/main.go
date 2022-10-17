f8b280d77b1077139abb03547b954b2205002b50
Sunny <2732981250@qq.com>
2022-10-17 10:53:17 +0800

    2.5fixed

--- main/main.go
+++ main/main.go
@@ -79,8 +79,8 @@ func connHandler(c net.Conn) {
 	myBlockchain := core.NewBlockchain(2)
 	//向区块链中先后添加6笔交易，每笔交易间延时1~3秒，模拟交易间的先后顺序
 	for i := 0; i < 18; i += 3 {
-		value, _ := strconv.Atoi(get[3])
-		myBlockchain.SendTransaction(get[0], get[1], value)
+		value, _ := strconv.Atoi(get[i+2])
+		myBlockchain.SendTransaction(get[i], get[i+1], value)
 		time.Sleep(time.Duration(rand.Intn(3)+1) * time.Second)
 	}
 	//打印区块链中的交易历史
